a5a4896f33a6f9548a547ec3658be78acc5d8c48
LUOMI <2025197335@qq.com>
2023-02-28 01:19:14 +0800

    hot-fix commit2

--- src/com/atguigu/git/GitTest.java
+++ src/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello,github!");
         System.out.println("hello,gitee3!");
         System.out.println("hello,gitee4!");
+        System.out.println("hot-fix test");
     }
 }
